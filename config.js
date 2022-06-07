36159ef2e00e9fa5daed9456d048f9471c91094d
NguyenTheDat218 <89387065+NguyenTheDat218@users.noreply.github.com>
2022-06-07 18:02:16 +0700

    upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "Love confess",
+    introTitle: 'Who are you',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Cho anh xin lỗi nha nha nha 🥰',
+    desc: '',
+    btnYes: 'Đồng ý <33',
+    btnNo: 'Không đồng ý :3',
+    question: 'Có thiệt không đó bà kia?',
+    btnReply: 'Gửi câu trả lời <3',
+    reply: 'Bởi vì em yêu anh nên em tha thứ đó',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    btnAccept: 'Hehehehe :)) <3',
+}
